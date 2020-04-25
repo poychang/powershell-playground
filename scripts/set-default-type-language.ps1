a2da86cec6eafc1847387baa8ddcbc154902a8a2
Poy Chang <poypost@gmail.com>
2020-04-25 11:33:26 +0800

    Update set-default-type-language.ps1

--- scripts/set-default-type-language.ps1
+++ scripts/set-default-type-language.ps1
@@ -2,7 +2,10 @@
 將 Windows 預設輸入法改成以 ENG (英文) 為主要輸入法，此模式很建議開發者使用
 #>
 
+# 增加英文語系
 $UserLanguageList = New-WinUserLanguageList -Language "zh-TW"
 $UserLanguageList.Add("en-US")
 Set-WinUserLanguageList -LanguageList $UserLanguageList -Force
+
+# 將預設輸入法改成以 ENG (英文) 為主要輸入法
 Set-WinDefaultInputMethodOverride -InputTip "0409:00000409"
