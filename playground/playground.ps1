6b8b811add8c6f1ff7262f3f091f73da16b546a9
Poy Chang <poypost@gmail.com>
2020-06-17 17:58:41 +0800

    update

--- playground/playground.ps1
+++ playground/playground.ps1
@@ -1,5 +1,11 @@
-Import-Module -Name "..\modules\output-module.ps1"
+Import-Module -Name "..\modules\output-module.psm1"
 
 Write-ColorOutput green "Hello"
 Write-ColorOutput yellow "Hello"
 Write-ColorOutput red "Hello"
+
+Write-Host "---------- Direct to Host ----------"
+
+Write-Output "Output"
+Write-Warning "Warning"
+Write-Error "Error"
